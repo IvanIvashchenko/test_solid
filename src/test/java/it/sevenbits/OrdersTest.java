8a4ca0072c48241480c75326ee2f58bc750802ce
Ivan Ivashchenko <ivan.ivashchenko@fairdevteam.com>
2013-10-10 17:37:48 +0700

    registration tests

--- src/test/java/it/sevenbits/OrdersTest.java
+++ src/test/java/it/sevenbits/OrdersTest.java
@@ -8,7 +8,12 @@ import org.openqa.selenium.WebElement;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
+import static org.fest.assertions.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.testng.Assert.assertNull;
 
 public class OrdersTest extends TestBase {
 
@@ -28,10 +33,57 @@ public class OrdersTest extends TestBase {
         element = this.findWebElement(By.xpath("/html/body/div[1]/div/div[2]/form/button"));
         assertNotNull(element);
         element.click();
+        element = this.findWebElement(By.className("alert"));
 //        WebDriverWait wait = new WebDriverWait(driver, 5);
 //        Alert alert = wait.until(ExpectedConditions.alertIsPresent());
 //        this.closeAlertAndGetItsText();
 
+    }
+
+    @Test
+    public void testValidRegistration() {
+
+        WebElement element = this.findWebElement(By.linkText("Регистрация"));
+        assertNotNull(element);
+        element.click();
+        element = this.findWebElement(By.id("user_nickname"));
+        assertNotNull(element);
+        element.sendKeys("test_user");
+        element = this.findWebElement(By.id("user_email"));
+        assertNotNull(element);
+        element.sendKeys("test_mail@test.com");
+        element = this.findWebElement(By.id("user_password"));
+        assertNotNull(element);
+        element.sendKeys("test_password");
+        element = this.findWebElement(By.xpath("//div[@class='block']/fieldset/div[@class='submit']/input"));
+        assertNotNull(element);
+        element.click();
+        assertEquals(driver.getCurrentUrl(), baseUrl);
+    }
+
+    @Test
+    public void testInvalidRegistration() {
+
+        WebElement element = this.findWebElement(By.linkText("Регистрация"));
+        assertNotNull(element);
+        element.click();
+        assertNull(this.findWebElement(By.className("help-inline")));
+
+        element = this.findWebElement(By.id("user_nickname"));
+        assertNotNull(element);
+        element.sendKeys("");
+        element = this.findWebElement(By.id("user_email"));
+        assertNotNull(element);
+        element.sendKeys("");
+        element = this.findWebElement(By.id("user_password"));
+        assertNotNull(element);
+        element.sendKeys("");
+        element = this.findWebElement(By.xpath("//div[@class='block']/fieldset/div[@class='submit']/input"));
+        assertNotNull(element);
+        element.click();
+        assertEquals(driver.getCurrentUrl(), baseUrl + "users");
+        List<WebElement> elements= driver.findElements(By.className("help-inline"));
+        assertThat(elements).isNotNull().hasSize(3);
     }
 
     private WebElement findWebElement(By by) {
